eb4c99df3fd0611eecd6d472f121f619b86b2e81
黄平 <10105186+yellow-can@user.noreply.gitee.com>
2023-01-08 18:13:22 +0800

    'redux'

--- src/constants/context.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { ILoginResult } from '../apis/types/login'
-import { SESSION_LOCAL_KEY } from './keys'
-
-// export const LoginContext = React.createContext<LoginState>(initialLoginState)
